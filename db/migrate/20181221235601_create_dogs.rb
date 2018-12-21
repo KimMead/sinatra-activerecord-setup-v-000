aea0b496dc6c0a31a6f5537524585777e5091bd3
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-12-21 23:58:21 +0000

    Automatically backed up by Learn

--- db/migrate/20181221235601_create_dogs.rb
+++ db/migrate/20181221235601_create_dogs.rb
@@ -1,12 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
   def up
     create_table :dogs do |t|
-      t.string :name 
-      t.string :breed 
-    end 
+      t.string :name
+      t.string :breed
+    end
   end
 
   def down
-    drop_table :dogs 
+    drop_table :dogs
   end
 end
